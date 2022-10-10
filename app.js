66da0edf5201a4c966aae7883f0e6f81659b0e59
Anant Tyagi <anant21100008@akgec.ac.in>
2022-10-10 19:17:14 +0530

    Final Commit

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ function clearErrors(){
 form.addEventListener('submit', (e)=>{
     e.preventDefault();
     console.log("First Name : "+form.fname.value)
-    console.log("First Name : "+form.fname.value)
+    console.log("First Name : "+form.flname.value)
     console.log("Phone Number : "+form.fphone.value)
     console.log("Email : "+form.femail.value)
     console.log("Password : "+form.fpass.value)
